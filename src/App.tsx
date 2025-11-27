b1299cf24c5f594a3c7daebba247cd23fac5bc96
Ukendiran <ukimanimaran@gmail.com>
2025-11-27 22:33:20 +0530

    Updated Target cursor

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,12 @@ import ExperienceSection from './components/ExperienceSection';
 import TestimonialsSection from './components/TestimonialsSection';
 import ContactSection from './components/ContactSection';
 import Footer from './components/Footer';
+import TargetCursor from './components/TargetCursor';
 
 function App() {
   return (
     <div className="bg-black min-h-screen">
+      <TargetCursor spinDuration={2} hideDefaultCursor={true} parallaxOn={true} />
       <Header />
       <main>
         <HeroSection />
